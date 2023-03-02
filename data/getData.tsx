34649eec2c4ace4a5e80986d1bb6d4bedf7822c0
nidaxnido <nidabelajar@gmail.com>
2023-03-02 23:06:13 +0700

    displayin data

--- data/getData.tsx
+++ data/getData.tsx
@@ -2,7 +2,7 @@ import fs from 'fs'
 import path from 'path'
 
 export async function getData() {
-    const fullPath = path.join(process.cwd(), "datadata.json")
+    const fullPath = path.join(process.cwd(), "data\\data.json")
 
     const content = fs.readFileSync(fullPath, "utf8")
     return JSON.parse(content)
